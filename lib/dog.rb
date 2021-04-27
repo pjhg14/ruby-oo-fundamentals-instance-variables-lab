243b6b636ccb91099c2af6a1e6c3c834319c0d96
pj <pjhg14@gmail.com>
2021-04-27 19:10:22 -0400

    commit tree 3900268e67fb1f28476d8beb776b13ea41db4594

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    def initialize
+        @this_dogs_name = "Lassie"
+    end
+
+    def name
+        @this_dogs_name
+    end
+
+    def name=(name)
+        @this_dogs_name = name
+    end
+end
